bb798cdba01cf9a48e5f3524d98174cce6e96fbe
0800 <spmspy0800@gmail.com>
2026-06-06 20:15:48 +0200

    Update pgis3-chat.js

--- javascript/pgis3-chat.js
+++ javascript/pgis3-chat.js
@@ -1,15 +1,2 @@
 console.log("script working");
   
-(function () {
-    if (window.self !== window.top) {
-        const paramName = "hide_badge";
-        const paramValue = true;
-
-        const url = new URL(window.location.href);
-
-        if (!url.searchParams.has(paramName)) {
-            url.searchParams.set(paramName, paramValue);
-            history.replaceState({}, "", url.toString());
-        }
-    }
-})();
